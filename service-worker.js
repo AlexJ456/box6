dbb8b7a33b3d0498d6795727de3c7dbf47b6240d
AlexJ456 <alexjohanson22@gmail.com>
2025-03-05 08:55:10 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
-const CACHE_NAME = 'box-breathing-cache-v2';
+const CACHENAME = 'box-breathing-cache-v2';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.json',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png',
-  '/icons/apple-icon-152x152.png',
-  '/icons/apple-icon-167x167.png',
-  '/icons/apple-icon-180x180.png'
+  'index.html',
+  'app.js',
+  'manifest.json',
+  'icons/icon-72x72.png',
+  'icons/icon-96x96.png',
+  'icons/icon-128x128.png',
+  'icons/icon-144x144.png',
+  'icons/icon-152x152.png',
+  'icons/icon-192x192.png',
+  'icons/icon-384x384.png',
+  'icons/icon-512x512.png',
+  'icons/apple-icon-152x152.png',
+  'icons/apple-icon-167x167.png',
+  'icons/apple-icon-180x180.png'
 ];
 
 // Install event - cache assets
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(CACHENAME)
       .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
@@ -30,7 +30,7 @@ self.addEventListener('install', event => {
 
 // Activate event - clean up old caches
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
+  const cacheWhitelist = [CACHENAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -67,7 +67,7 @@ self.addEventListener('fetch', event => {
             // Clone the response
             const responseToCache = response.clone();
             
-            caches.open(CACHE_NAME)
+            caches.open(CACHENAME)
               .then(cache => {
                 cache.put(event.request, responseToCache);
               });
@@ -77,7 +77,7 @@ self.addEventListener('fetch', event => {
           .catch(() => {
             // If fetch fails, return the offline page for navigation requests
             if (event.request.mode === 'navigate') {
-              return caches.match('/index.html');
+              return caches.match('index.html');
             }
           });
       })
